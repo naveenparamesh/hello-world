83648a4fe6eb884bc7287bc6ea5811ca38572697
naveenparamesh <npara@udel.edu>
2015-09-03 14:25:43 +0000

    ..

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ int main(){
     std::cout << "This is cool!" << std::endl;
     std::cout << "Like this is friggin cool!" << std::endl;
     std::cout << "I'm speechless!" << std::endl;
+    std::cout << "OMG!!!!" << std::endl;
     return 0;
 }
